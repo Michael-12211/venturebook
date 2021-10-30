c81e6e879b950a14cdf7f4b519d18e4a2785c163
Michael-12211 <michaeldavidkempe@gmail.com>
2021-10-29 20:06:17 -0400

    Initial Commit

---
+++ VentureBook/VentureBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentureBookApp.swift
+//  VentureBook
+//
+//  Created by Michael Kempe on 2021-10-29.
+//
+
+import SwiftUI
+
+@main
+struct VentureBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VentureBookTests/VentureBookTests.swift
@@ -0,0 +1,33 @@
+//
+//  VentureBookTests.swift
+//  VentureBookTests
+//
+//  Created by Michael Kempe on 2021-10-29.
+//
+
+import XCTest
+@testable import VentureBook
+
+class VentureBookTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
