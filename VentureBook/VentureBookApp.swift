21d94b57ddccea70014aefaf56cb93707e8f1f5c
Michael-12211 <michaeldavidkempe@gmail.com>
2021-11-02 20:05:51 -0400

    Fixed error establishing environment objects from helper classes

--- VentureBook/VentureBookApp.swift
+++ VentureBook/VentureBookApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct VentureBookApp: App {
+    
+    let persistenceController = PersistenceController.shared
+    let noteCDBHelper = NoteCDBHelper(context: PersistenceController.shared.container.viewContext)
+    let tripCDBHelper = TripCDBHelper(context: PersistenceController.shared.container.viewContext)
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(noteCDBHelper).environmentObject(tripCDBHelper)
         }
     }
 }
