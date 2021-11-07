2f1a2dbe37f339c8aab1bb8b732f3c65a1022038
Michael-12211 <michaeldavidkempe@gmail.com>
2021-11-06 22:12:21 -0400

    added trip picker

--- VentureBook/Views/AddNote.swift
+++ VentureBook/Views/AddNote.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct AddNote: View {
+    
+    @EnvironmentObject var noteCDBHelper : NoteCDBHelper
+    @EnvironmentObject var tripCDBHelper : TripCDBHelper
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var title = "1"
+    @State private var description = "1"
+    @State private var trip = "none"
+    @State private var picture: Data? = nil
+    
+    @State private var trips = [String]()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack
+            {
+                Text("Trip:")
+                    .padding(.leading, 50.0)
+                Picker("Please choose trip", selection: $trip){
+                ForEach(trips, id: \.self) {
+                    Text($0)
+                }
+                    
+            }
+            .frame(height: 250.0)
+                .pickerStyle(WheelPickerStyle())                    }
+            .navigationBarTitle("Place Reservation", displayMode: .inline)
+        }
+        .onAppear(){
+            self.tripCDBHelper.getAllTrips()
+            if (tripCDBHelper.mTrips.count > 0)
+            {
+                for nTrip in tripCDBHelper.mTrips
+                {
+                    trips.append(nTrip.title)
+                }
+                trip = tripCDBHelper.mTrips[0].title
+            }
+            else
+            {
+                trips.append("none")
+            }
+        }
     }
 }
 
--- VentureBook/Views/ContentView.swift
+++ VentureBook/Views/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 }) {
                     NavigationLink("Make a note", destination: AddNote())
                 }
+                .navigationBarTitle("Home")
             }
         }
     }
