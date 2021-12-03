b3d7d46809ed076a1452223c2ed648a1d0e37f4e
Kevin <kevintran1090@gmail.com>
2021-12-02 23:41:58 -0500

    temp1

--- VentureBook/Views/MapView.swift
+++ VentureBook/Views/MapView.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 import MapKit
 
-struct MapsView: View {
+struct MapView: View {
+    @EnvironmentObject var noteCDBHelper : NoteCDBHelper
     @EnvironmentObject var locationHelper : LocationHelper
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locationHelper.currentLocation!.coordinate.latitude, longitude: locationHelper.currentLocation!.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 43.46909205277883, longitude: -79.69961738210894), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+        Map(coordinateRegion: $region, showsUserLocation: true,  userTrackingMode: .constant(.follow), annotationItems: self.noteCDBHelper.mNotes.map{
+            i in
+            i.convertToNote()
+        }){
+            return self.locationHelper.doGeocoding(address: $0.location, completionHandler: { (address, error) in
+                
+                if (error == nil && address != nil){
+                    //sucessfully obtained coordinates
+                    let coordinate = address!
+                    MapPin(coordinate: coordinate, tint: Color.blue)
+                    print(#function, "Address obtained : \(address!)")
+                    
+                }
+            })
+        }
                     .frame(width: 400, height: 300)
+                    .onAppear(){
+                        self.noteCDBHelper.getAllNotes()
+                        self.noteCDBHelper.mNotes.forEach{
+                            i in
+                            
+                        }
+                    }
     }
+        
 }
 
-struct MapsView_Previews: PreviewProvider {
+struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapsView()
+        MapView()
     }
 }
